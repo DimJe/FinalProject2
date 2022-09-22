3ce1f71e0033f736bb9f0d5e1063573bc3dffca5
Dimje <mark759@naver.com>
2022-09-22 15:19:31 +0900

    initView

--- app/src/main/java/org/techtown/finalproject2/MakeUser.kt
+++ app/src/main/java/org/techtown/finalproject2/MakeUser.kt
@@ -2,19 +2,56 @@ package org.techtown.finalproject2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import org.techtown.finalproject2.databinding.ActivityMainBinding
 import org.techtown.finalproject2.databinding.ActivityMakeUserBinding
+import org.techtown.finalproject2.makeUser.MakeUser1
+import org.techtown.finalproject2.makeUser.MakeUser2
+import org.techtown.finalproject2.makeUser.MakeUser3
 
 class MakeUser : AppCompatActivity() {
 
     private val binding : ActivityMakeUserBinding by lazy {
         ActivityMakeUserBinding.inflate(layoutInflater)
     }
-
+    var flag = 0
+    private val transaction = supportFragmentManager.beginTransaction()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        transaction.add(R.id.container, MakeUser1())
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+        binding.next.setOnClickListener {
+
+            if(flag==2){
+                //
+            }
+            else {
+                switchFragment()
+                Log.d("태그", "switchFragment")
+            }
+        }
 
     }
+
+    private fun switchFragment() {
+        val transaction = supportFragmentManager.beginTransaction()
+        when(flag){
+            0 -> {
+                transaction.add(R.id.container, MakeUser2())
+                flag = 1
+            }
+            1 -> {
+                transaction.replace(R.id.container, MakeUser3())
+                flag = 2
+            }
+        }
+        transaction.addToBackStack(null)
+        transaction.commit()
+
+    }
+
 }
--- app/src/main/java/org/techtown/finalproject2/User.kt
+++ app/src/main/java/org/techtown/finalproject2/User.kt
@@ -1,8 +1,10 @@
 package org.techtown.finalproject2
 
-class User {
+data class User(val userName : String, val userNickName : String,val birth : String,
+                val userPhoneNumber : String, val gender : Char, val length : Int,
+                val weight : Int, val region : String, val type : String,
+                val sports : ArrayList<String>, val team : String, val career : String
+) {
 
-    constructor(userName : String,userNickName : String,birth : String,userPhoneNumber : String,gender : Char,){
 
-    }
 }
